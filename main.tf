0c04ec66162370535c873a611c105e9038f6135a
Marysabel Aedo Moreno <105820972+MaryAedo@users.noreply.github.com>
2026-05-30 12:09:55 -0400

    modificacion de llamado a repos externos de vpc y ec2

--- main.tf
+++ main.tf
@@ -1,25 +1,18 @@
 # ==============================================================================
-
 # Evaluación Parcial 2 - Infraestructura como Código II (AUY1105)
-
 # Orquestador Raíz de Infraestructura Modularizada
-
 # ==============================================================================
 
-
-
 module "redes" {
-  # checkov:skip=CKV_TF_1: Pruebas en rama dev
-  # checkov:skip=CKV_TF_2: Pruebas en rama dev
-  source = "git::https://github.com/Solange-sm/terraform-aws-vpc-AUY1105-Grupo-4.git//vpc_module?ref=dev-sm"
+  # Apuntando a la versión semántica oficial exigida por la rúbrica
+  source = "git::https://github.com/Solange-sm/terraform-aws-vpc-AUY1105-Grupo-4.git//vpc_module?ref=v1.0.0"
   
   mi_ip_acceso = var.mi_ip_acceso
 }
 
 module "computo" {
-  # checkov:skip=CKV_TF_1: Pruebas en rama dev
-  # checkov:skip=CKV_TF_2: Pruebas en rama dev
-  source = "git::https://github.com/Solange-sm/terraform-aws-EC2-AUY1105-Grupo-4.git?ref=dev-ma"
+  # Apuntando a la versión semántica oficial exigida por la rúbrica
+  source = "git::https://github.com/Solange-sm/terraform-aws-EC2-AUY1105-Grupo-4.git?ref=v1.0.0"
   
   subnet_id         = module.redes.public_subnet_ids[0]
   security_group_id = module.redes.security_group_id
