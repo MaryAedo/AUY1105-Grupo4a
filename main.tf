06412ba9cefdeca4608ac771ce89a3928176b06c
Solange-sm <sol.milla@duocuc.cl>
2026-06-27 10:10:24 -0400

    Update module source references to V2.0.0

--- main.tf
+++ main.tf
@@ -5,14 +5,14 @@
 
 module "redes" {
   # Apuntando a la versión semántica oficial exigida por la rúbrica
-  source = "git::https://github.com/Solange-sm/terraform-aws-vpc-AUY1105-Grupo-4.git//vpc_module?ref=v2.0.0"
+  source = "git::https://github.com/Solange-sm/terraform-aws-vpc-AUY1105-Grupo-4.git//vpc_module?ref=V2.0.0"
   
   mi_ip_acceso = var.mi_ip_acceso
 }
 
 module "computo" {
   # Apuntando a la versión semántica oficial exigida por la rúbrica
-  source = "git::https://github.com/Solange-sm/terraform-aws-EC2-AUY1105-Grupo-4.git?ref=v2.0.0"
+  source = "git::https://github.com/Solange-sm/terraform-aws-EC2-AUY1105-Grupo-4.git?ref=V2.0.0"
   
   subnet_id         = module.redes.public_subnet_ids[0]
   security_group_id = module.redes.security_group_id
